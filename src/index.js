199a205ed2ef4b217e8bcfec1d200240fa65103e
Gus Suarez <gus.suarez@zeeto.io>
2017-02-24 00:41:37 -0800

    added redux + router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
+// rename to root reducers, move to root dir
+import reducers from './reducers';
+
 import App from './App';
 import './index.css';
 
-const Application = <App />;
+const store = createStore(combineReducers({
+  ...reducers,
+  routing: routerReducer
+}));
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+const Application = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>
+);
+
+
 ReactDOM.render(
   Application,
   document.getElementById('root')
